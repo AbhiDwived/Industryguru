aca65df22c1bc7a1a9aaf26ad17c33e8789e46d6
Abhinandan <abhinandan@mybestvenue.com>
2025-08-28 16:57:02 +0530

    url update

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,3 +1,3 @@
-// export const apiLink = "https://industryguru-backend.hcx5k4.easypanel.host/";
-export const apiLink = "http://localhost:8001";
+export const apiLink = "https://industryguru-backend.hcx5k4.easypanel.host/";
+// export const apiLink = "http://localhost:8001";
 
