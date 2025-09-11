b155120a65918a57a4b8bd24b76fa203d3c44352
Abhinandan <abhinandan@mybestvenue.com>
2025-09-11 22:48:58 +0530

    feat(config): add allowed hosts for preview
    Adds a list of allowed hosts to the vite config to enable
    access from different environments. This includes localhost,
    127.0.0.1, 0.0.0.0, industryguru-frontend-fah7v9-71df13-147-93-18-171.traefik.me,
    and industryguru.in.

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,13 @@ export default {
   preview: {
     port: 3000,
     host: '0.0.0.0',
-    allowedHosts: 'all'
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      '0.0.0.0',
+      'industryguru-frontend-fah7v9-71df13-147-93-18-171.traefik.me',
+      'industryguru.in'
+    ]
   },
   build: {
     outDir: 'dist'
